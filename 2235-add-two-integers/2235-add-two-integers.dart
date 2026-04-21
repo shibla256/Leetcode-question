caef015cdf786c902a8121d0bde3fcdf4405121b
FATHIMA SHIBILA M A <shibla256@gmail.com>
2026-04-21 20:42:06 +0530

    Time: 3 ms (0%), Space: 148.5 MB (50%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.dart
@@ -0,0 +1,15 @@
+class Solution {
+  int sum(int num1, int num2) {
+    int result=num1+num2;
+    print("the sum is $sum");
+    return result;
+    
+  
+}
+  void main(){
+
+ Solution sn=Solution();
+ sn.sum(12,5);
+  
+}
+}
